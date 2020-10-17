d0df9dedbce3d4b8e46709ba15c2d25c16923e74
Roherim <64076990+Roherim@users.noreply.github.com>
2020-10-17 17:30:10 +0300

    Сайт стандес
    практикуюсь в верстке, первый сайт ручками

---
+++ Сайт/index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+	<meta charset="UTF-8">
+	<title>Стандес</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+	<h1>STANDES</h1>
+	
+</body>
+</html>
